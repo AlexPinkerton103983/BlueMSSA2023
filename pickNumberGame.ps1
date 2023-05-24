f8c38767a6a989440d5d24187c40b2161b3f9f97
Alex Pinkerton <S103983@student.aiict.edu.au>
2023-05-23 17:23:55 -0700

    Updated to VS Code

---
+++ pickNumberGame.ps1
@@ -0,0 +1,8 @@
+﻿
+$NumberToGuess = 1..100 | Get-Random
+do {
+  $UserGuess = Read-Host -Prompt "Please enter a number between 1 and 100"
+  if ($UserGuess -lt $NumberToGuess) {write-host "Pick a higher number"}
+  elseif ($UserGuess -gt $NumberToGuess) {write-host "Pick a lower number"}
+  else {write-host "Congrats! You picked the right number"}
+} until ($NumberToGuess -eq $UserGuess)
