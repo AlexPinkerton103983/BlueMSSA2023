0756f8de1cd807d3a90caafa8d51bbc41ab57ed0
Alex Pinkerton <S103983@student.aiict.edu.au>
2023-05-29 15:59:55 -0700

    Week 2 Monday Update

--- Lab01.ps1
+++ Lab01.ps1
@@ -5,18 +5,15 @@ get-command *dns*
 #ANSWER
 Resolve-DnsName
 
-# What command would you run to make changes to a network adapter?
-# After finding such a command, what parameter would you use to change
-# it's Media Access Control (MAC) address (on adapters that support changes to their
-# MAC)Address?
+<# What command would you run to make changes to a network adapter? After finding such a command, what parameter would you use to change its Media Access Control (MAC) address (on adapters that support changes to their MAC address)? #>
 get-command *network*
 get-command -noun networkadapter
 get-command *adapter*
 # ANSWER
 Set-NetAdapter
 
-get-help set-netadapter
-Set-NetAdapter -MacAddress letters -Whatif
+get-help set-netadapter -showwindow
+Set-NetAdapter -MacAddress aabbccddeeff -Whatif
 
 
 # What command would let you enable a previously disabled scheduled task?
@@ -24,17 +21,23 @@ get-command *scheduled*
 # ANSWER
 Enable-ScheduledTask
 
-#What command would let you block access to a file shared by a particular user?
+# What command would let you block access to a file share by a particular user?
 get-command *block*
+get-help block-fileshareaccess -ShowWindow
 # ANSWER
-get-help block-smbshareaccess
+get-help block-smbshareaccess -ShowWindow
 
 # What command would you run to clear your computer's local BranchCache cache?
 get-command *branchcache*
 get-command -verb clear
 get-help clear-bccache -ShowWindow
 # ANSWER
-Clear-BCCache
+Clear-BCCache -WhatIf
+
+<# What command would you run to display a list of Windows Firewall rules? What parameter of that command would display only enabled rules? #>
+get-command -noun firewall
+Get-Command *firewall*
+get-help get-netfirewallfule -ShowWindow
 
 
 
